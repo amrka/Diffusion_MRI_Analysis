0f666825f17ff3c88ab45b0ae8aa90a108dfa6ee
amrka <amr.kamel@uv.es>
2020-08-06 13:29:35 +0200

    allocate 32G for each task

--- DTI_correlation_analysis.py
+++ DTI_correlation_analysis.py
@@ -252,6 +252,6 @@ DTI_corr.connect ([
 
 DTI_corr.write_graph(graph2use='colored', format='svg', simple_form=True)
 DTI_corr.run(plugin='SLURM', plugin_args={'dont_resubmit_completed_jobs': True,'max_jobs':50,
-'sbatch_args':'--mem=16G'})
+'sbatch_args':'--mem=32G'})
 # plugin_args={'sbatch_args': '--time=24:00:00 -N1 -c2 --mem=40G','max_jobs':200}
 # DTI_corr.run('MultiProc', plugin_args={'n_procs': 8})
