d6805a4aa40aa3a4a8b5bb0ec7ea36960aa78756
Amr <amr.kamel@uv.es>
2020-09-22 14:12:00 +0200

    add the output directory and stat name to the outputs

--- create_wmfod_figures.sh
+++ create_wmfod_figures.sh
@@ -14,13 +14,14 @@
 # >>> 2     # colormap (here blue-lightblue)
 
 
-cd /Users/amr/Dropbox/thesis/diffusion/to_wmfod_template/
+dir='/Users/amr/Dropbox/thesis/diffusion/to_wmfod_template/'
 
 
 fsleyes='pythonw /Users/amr/anaconda3/bin/fsleyes' #aliases do not work inside scripts
 wmfod_template='/Users/amr/Dropbox/thesis/registration/wmfod_template.nii.gz'
 wmfod_template_skeleton='/Users/amr/Dropbox/thesis/registration/wmfod_template_skeleton.nii.gz'
 stat_map=`remove_ext $1`
+stat_map_name=`echo ${stat_map} | sed  s/.*_map_id_// | sed s[/palm.*[[`
 domain=$2   #behavior or tbss
 
 # colormap -> $3  #red-yellow or blue-lightblue
@@ -39,26 +40,26 @@ for slice_no in {56,51,46,41,36,31,26,21};do
 
 	${fsleyes} render  \
 	--scene ortho -no   --displaySpace world --hidex --hidey    -vl 36 42 ${slice_no} \
-	--hideCursor   --outfile    ${stat_map}_${domain}_render_${slice_no}.png   \
+	--hideCursor   --outfile    ${dir}/${stat_map_name}_${domain}_render_${slice_no}.png   \
 	${wmfod_template} --displayRange 0.04 0.35  \
 	${wmfod_template_skeleton} --cmap green --displayRange 0.2 0.3 \
 	${stat_map} --displayRange 0.95  1 --cmap ${colormap}
 
-	convert  ${stat_map}_${domain}_render_${slice_no}.png -crop 320x220+245+185  ${stat_map}_${domain}_render_cropped_${slice_no}.png
+	convert  ${dir}/${stat_map_name}_${domain}_render_${slice_no}.png -crop 320x220+245+185  ${dir}/${stat_map_name}_${domain}_render_cropped_${slice_no}.png
 
 done
 
 
 pngappend \
-${stat_map}_${domain}_render_cropped_21.png + \
-${stat_map}_${domain}_render_cropped_26.png + \
-${stat_map}_${domain}_render_cropped_31.png - \
-${stat_map}_${domain}_render_cropped_36.png + \
-${stat_map}_${domain}_render_cropped_41.png + \
-${stat_map}_${domain}_render_cropped_46.png + \
-${stat_map}_${domain}_render_cropped_51.png + \
-${stat_map}_${domain}_render_cropped_56.png   \
-${stat_map}_${domain}_output.png
+${dir}/${stat_map_name}_${domain}_render_cropped_21.png + \
+${dir}/${stat_map_name}_${domain}_render_cropped_26.png + \
+${dir}/${stat_map_name}_${domain}_render_cropped_31.png - \
+${dir}/${stat_map_name}_${domain}_render_cropped_36.png + \
+${dir}/${stat_map_name}_${domain}_render_cropped_41.png + \
+${dir}/${stat_map_name}_${domain}_render_cropped_46.png + \
+${dir}/${stat_map_name}_${domain}_render_cropped_51.png + \
+${dir}/${stat_map_name}_${domain}_render_cropped_56.png   \
+${dir}/${stat_map_name}_${domain}_output.png
 
 # remove background
-convert ${stat_map}_${domain}_output.png -transparent black ${stat_map}_${domain}_output_no_bg.png
+convert ${dir}/${stat_map_name}_${domain}_output.png -transparent black ${dir}/${stat_map_name}_${domain}_output_no_bg.png
