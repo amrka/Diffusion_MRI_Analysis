083462fc7aba3422799211b97eb1f770300de485
amrka <amr.kamel@uv.es>
2021-04-16 17:46:30 +0200

    get tbss_sig maps to make study_temp figures

--- temp_script.sh
+++ temp_script.sh
@@ -129,3 +129,32 @@
 # ./create_wmfod_figures.sh \
 # /Users/amr/Dropbox/thesis/diffusion/DTI_corr/OF_total_distance.con_OF_total_distance.mat/Kurtosis_TORT_P_value1.nii.gz \
 # behavior
+
+###################################################################################
+# using FA study based template
+# TBSS
+# first get the maps with significant values
+cd /Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study
+for img in `ls */palm_tbss/*_tbss_tfce_tstat_fwep_c*.nii.gz`;do
+    sig=`fslstats $img -p 100`;
+    if [[ "${sig}" > 0.949 ]];then
+    	echo $PWD/$img
+    fi
+done
+###################################################################################
+# result: tbss_maps with significant voxels
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_CHARMED_FA/palm_tbss/palm_tbss_tfce_tstat_fwep_c1.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_CHARMED_FA/palm_tbss/palm_tbss_tfce_tstat_fwep_c2.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_CHARMED_FR/palm_tbss/palm_tbss_tfce_tstat_fwep_c1.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_CHARMED_IAD/palm_tbss/palm_tbss_tfce_tstat_fwep_c1.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_CHARMED_IAD/palm_tbss/palm_tbss_tfce_tstat_fwep_c2.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_Diffusion_20_AD/palm_tbss/palm_tbss_tfce_tstat_fwep_c2.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_Diffusion_20_FA/palm_tbss/palm_tbss_tfce_tstat_fwep_c1.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_Diffusion_20_FA/palm_tbss/palm_tbss_tfce_tstat_fwep_c2.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_Diffusion_20_MD/palm_tbss/palm_tbss_tfce_tstat_fwep_c2.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_Diffusion_20_RD/palm_tbss/palm_tbss_tfce_tstat_fwep_c2.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_Kurtosis_E_DTI_FA/palm_tbss/palm_tbss_tfce_tstat_fwep_c1.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_Kurtosis_E_DTI_FA/palm_tbss/palm_tbss_tfce_tstat_fwep_c2.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_NODDI_FICVF/palm_tbss/palm_tbss_tfce_tstat_fwep_c1.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_NODDI_ODI/palm_tbss/palm_tbss_tfce_tstat_fwep_c1.nii.gz
+/Volumes/Amr_1TB/DTI_corr/Diffusion_TBSS_Stat/DTI_TBSS_workingdir_Study_Based_Template/DTI_TBSS_Study/_map_id_NODDI_ODI/palm_tbss/palm_tbss_tfce_tstat_fwep_c2.nii.gz
